641899a67e08522356b663045709018af283d02e
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-27 12:33:17 +0300

    fix: Define the functions with their associated loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,29 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 10
+    while count >= 0:
+        if count == 0:
+            print("Happy New Year!")
+        else:
+            print(count)
+        count -= 1
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list = [int * int for int in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(101):
+        if i > 0:
+            if i % 3 == 0 and i % 5 == 0:
+                print('FizzBuzz')
+            elif i % 3 == 0:
+                print('Fizz')
+            elif i % 5 == 0:
+                print('Buzz')
+            else:
+                print(i)
+    
